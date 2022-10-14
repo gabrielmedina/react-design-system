54da45ecf9627f8c6ab5cde63b7b6f3ab27d2717
Gabriel Medina <gmedina.santos@gmail.com>
2022-10-14 09:52:40 -0400

    docs(button): Meta and StoryObj types

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,8 +1,23 @@
-import { Button } from '.'
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, TButton } from '.'
 
 export default {
   title: 'Atoms/Button',
   component: Button,
-}
+  args: {
+    variant: 'primary',
+  },
+  argTypes: {
+    element: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<TButton>
 
-export const Default = () => <Button>Button</Button>
+export const Default: StoryObj<TButton> = {
+  args: {
+    children: 'Button',
+  },
+}
